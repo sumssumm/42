086845425356a4f6a37e749619e57d83553cc121
Sumin Park <suminpar@cx2r1s3.42seoul.kr>
2023-12-12 14:08:04 +0900

    start

---
+++ philosophers/philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suminpar <suminpar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 04:17:15 by suminpar          #+#    #+#             */
+/*   Updated: 2023/12/12 13:41:16 by suminpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h> //printf
+# include <stdlib.h> //malloc, free
+# include <string.h> //memset
+# include <unistd.h> //write, usleep 
+# include <pthread.h> //pthread
+# include <sys/time.h> //gettimeofday
+
+#endif
