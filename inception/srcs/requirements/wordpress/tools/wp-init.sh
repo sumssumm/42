f3f8d0e90d21f063c6a41f2abc5d2b2ffbf4f791
Sumin Park <suminpark@Suminui-MacBookAir.local>
2024-06-14 06:05:53 +0900

    modified dockerfile

--- inception/srcs/requirements/wordpress/tools/wp-init.sh
+++ inception/srcs/requirements/wordpress/tools/wp-init.sh
@@ -4,7 +4,21 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=wordpress \
+							--dbuser=wpuser \
+							--dbpass=password \
+							--dbhost=mariadb \
+							--allow-root
+./wp-cli.phar core install --url=localhost \
+							--title=inception \
+							--admin_user=admin \
+							--admin_password=admin \
+							--admin_email=admin@admin.com \
+							--allow-root
+./wp-cli.phar user create suminpar \
+							suminpar@ \
+							--user_pass=passwordd \
+							--role=author \
+							--allow-root
 
 exec "$@"
