6e646e3b38777585dc2cc9330cef48740f21f39b
Sumin Park <suminpar@c2r10s7.42seoul.kr>
2024-07-13 22:35:52 +0900

    ex00 finish

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "whatever.hpp"
+
+int main(void) {
+  int a = 2;
+  int b = 3;
+  std::cout << "before: " << "a = " << a << ", b = " << b << std::endl;
+
+  ::swap(a, b);
+  std::cout << "after: " << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  std::cout << "-------------------------------------" << std::endl;
+
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  std::cout << "before: " << "c = " << c << ", d = " << d << std::endl;
+
+  ::swap(c, d);
+  std::cout << "after: " << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+  std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+  std::cout << "-------------------------------------" << std::endl;
+
+  return 0;
+}
