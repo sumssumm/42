66903ae003661c40c92f035fce76952eab89b6bc
Sumin Park <suminpar@c2r10s7.42seoul.kr>
2024-07-13 22:46:33 +0900

    change min max return value to const T&

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -23,5 +23,15 @@ int main(void) {
   std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
   std::cout << "-------------------------------------" << std::endl;
 
+  float e = 42.42f;
+  float f = 21.21f;
+  std::cout << "before: " << "e = " << e << ", f = " << f << std::endl;
+
+  ::swap(e, f);
+  std::cout << "after: " << "e = " << e << ", f = " << f << std::endl;
+  std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
+  std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
+  std::cout << "-------------------------------------" << std::endl;
+
   return 0;
 }
