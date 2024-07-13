e0463786e364924eefd7984987bc8f1611cba838
Sumin Park <suminpar@c2r10s7.42seoul.kr>
2024-07-14 00:42:31 +0900

    edit main.cpp

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -23,12 +23,12 @@ int main(void) {
   std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
   std::cout << "-------------------------------------" << std::endl;
 
-  float e = 42.42f;
-  float f = 21.21f;
-  std::cout << "before: " << "e = " << e << ", f = " << f << std::endl;
+  const float e = 42.42f;
+  const float f = 21.21f;
+  // std::cout << "before: " << "e = " << e << ", f = " << f << std::endl;
 
-  ::swap(e, f);
-  std::cout << "after: " << "e = " << e << ", f = " << f << std::endl;
+  // ::swap(e, f);
+  // std::cout << "after: " << "e = " << e << ", f = " << f << std::endl;
   std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
   std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
   std::cout << "-------------------------------------" << std::endl;
