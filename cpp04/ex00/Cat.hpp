280b012449ebcb08f7f6dcfbecf3adc3a296b639
sumssumm <sumssum.p@gmail.com>
2024-08-29 12:03:03 +0900

    ex03...

--- cpp04/ex00/Cat.hpp
+++ cpp04/ex00/Cat.hpp
@@ -1,17 +1,16 @@
 #ifndef CAT_HPP
-# define CAT_HPP
+#define CAT_HPP
 
-# include "Animal.hpp"
+#include "Animal.hpp"
 
-class Cat : public Animal
-{
-	public:
-		Cat();
-		Cat(const Cat &cat);
-		Cat& operator=(const Cat &cat);
-		~Cat();
+class Cat : public Animal {
+ public:
+  Cat();
+  Cat(const Cat &cat);
+  Cat &operator=(const Cat &cat);
+  virtual ~Cat();
 
-		void makeSound() const;
+  void makeSound() const;
 };
 
 #endif
